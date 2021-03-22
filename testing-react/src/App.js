60f7389ad93f4957c8606f519c4414582b45a711
roiazran65@gmail.com <roiazran65@gmail.com>
2021-03-22 16:09:46 +0200

    test code checkbox functionality

--- testing-react/src/App.js
+++ testing-react/src/App.js
@@ -5,10 +5,18 @@ import './App.css';
 function App() {
   const [buttonColor, setButtonColor] = useState('red');
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+  const [disabled, setDisabled] = useState(false);
 
   return (
     <div>
-      <button onClick={()=>setButtonColor(newButtonColor)} style={{ backgroundColor: buttonColor }}>Change to {newButtonColor}</button>
+      <button disabled={disabled} onClick={()=>setButtonColor(newButtonColor)} style={{ backgroundColor: buttonColor }}>Change to {newButtonColor}</button>
+      <input 
+      onChange={(e)=>setDisabled(e.target.checked)} 
+      defaultChecked={disabled}
+      aria-checked={disabled}
+      type="checkbox" 
+      name="" id=""
+      />
     </div>
   );
 }
--- testing-react/src/App.test.js
+++ testing-react/src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial colot and chage color when clicked', () => {
-  render(<App/>);
+  render(<App />);
   const colorBtn = screen.getByRole('button', { name: 'Change to blue'});
   expect(colorBtn).toHaveStyle({ backgroundColor: 'red' });
   
@@ -11,4 +11,23 @@ test('button has correct initial colot and chage color when clicked', () => {
   expect(colorBtn.textContent).toBe('Change to red');
 });
 
+test('initial conditions', ()=>{
+  render(<App />);
+  const colorBtn = screen.getByRole('button', { name : 'Change to blue' });
+  expect(colorBtn).toBeEnabled();
 
+  const checkBox = screen.getByRole('checkbox');
+  expect(checkBox).not.toBeChecked();
+
+});
+
+test('button is disabled when checkbox is checked an enable when checkbox unchecked',()=>{
+  render(<App />);
+  const colorBtn = screen.getByRole('button');
+  const checkBox = screen.getByRole('checkbox');
+
+  fireEvent.click(checkBox);
+  expect(colorBtn).toBeDisabled();
+  fireEvent.click(checkBox);
+  expect(colorBtn).toBeEnabled();
+})
